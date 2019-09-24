c20efdb6ea1aeafe5bf4b4240524af64ce54c991
DPhamBCIT <dpham33@mybcit.ca>
2019-09-23 19:19:12 -0700

    I've finished my part

--- WindowsFormsApp2/Frequency.Designer.cs
+++ WindowsFormsApp2/Frequency.Designer.cs
@@ -31,6 +31,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -45,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 79);
+            this.button2.Location = new System.Drawing.Point(12, 255);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(133, 52);
             this.button2.TabIndex = 1;
@@ -55,24 +58,49 @@
             // 
             // panel1
             // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.hScrollBar1);
             this.panel1.Location = new System.Drawing.Point(151, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(303, 227);
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(599, 295);
             this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 232);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Remember size ?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(-3, 278);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(602, 17);
+            this.hScrollBar1.TabIndex = 0;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
             // 
             // Frequency
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 251);
+            this.ClientSize = new System.Drawing.Size(762, 319);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Frequency";
             this.Text = "Frequency";
             this.Load += new System.EventHandler(this.Frequency_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +109,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
     }
 }
